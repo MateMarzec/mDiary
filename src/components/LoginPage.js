b32185e27d48a32b4e899ebb145bd2b479ef35c5
MateMarzec <64321779+MateMarzec@users.noreply.github.com>
2022-03-10 00:47:33 +0000

    CSS Styles
    -Logo Styles Created
    -Login Page Styles Done
    -Fonts Imported
    -Base Style Created

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,10 +5,10 @@ import { googleLogin, facebookLogin } from '../actions/auth';
 export const LoginPage = ({ googleLogin, facebookLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Hello!</h1>
-      <h3 className="box-subtitle">Login to Your Account</h3>
-      <button className="button" onClick={googleLogin}>Login with Google</button>
-      <button className="button" onClick={facebookLogin}>Login with Facebook</button>
+      <h1 className="login_item box-layout__title">Hello!</h1>
+      <h3 className="login_item box-subtitle">Login to Your Account</h3>
+      <button className="login_item login-form-font login__google" onClick={googleLogin}>Login via Google</button>
+      <button className="login_item login-form-font login__facebook" onClick={facebookLogin}>Login with Facebook</button>
     </div>
   </div>
 );
